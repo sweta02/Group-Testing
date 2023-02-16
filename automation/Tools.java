94a20ede8176cc7c4fbea9c58d766b18e5251731
sweta patel <sweta_0279@yahoo.co.uk>
2023-02-16 22:09:14 +0000

    new code

---
+++ automation/Tools.java
@@ -0,0 +1,8 @@
+package automation;
+
+public class Tools {
+    public static void main(String[] args) {
+        System.out.println(" Selenium");
+        System.out.println(" Silk Test");
+    }
+}
---
+++ platform/Program.java
@@ -0,0 +1,9 @@
+package platform;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Java Oak");
+        System.out.println(" Java Playground");
+
+    }
+}
